c71b0d14612d40d4a18dffb87abbbd2db3918d53
gabrielkumagai <gagabriel937@gmial.com>
2025-01-08 11:57:16 -0300

    projeto aula 4, final do dia

--- migrations/Version20250107110301.php
+++ migrations/Version20250107110301.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 declare(strict_types=1);
 
 namespace DoctrineMigrations;
@@ -7,22 +7,21 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20250107110301 extends AbstractMigration
 {
     public function getDescription(): string
     {
         return '';
     }
-
+    
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE episode ADD COLUMN watched BOOLEAN DEFAULT FALSE');
-
+        
     }
-
-
+    
+    
 }
+*/
